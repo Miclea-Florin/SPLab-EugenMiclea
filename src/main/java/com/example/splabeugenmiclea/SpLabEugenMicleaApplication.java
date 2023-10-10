2c3947af1335db7b7d4632c8aa6825628944d418
Miclea-Florin <95932163+Miclea-Florin@users.noreply.github.com>
2023-10-10 08:34:35 +0300

    initialized spring project

---
+++ src/main/java/com/example/splabeugenmiclea/SpLabEugenMicleaApplication.java
@@ -0,0 +1,13 @@
+package com.example.splabeugenmiclea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpLabEugenMicleaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpLabEugenMicleaApplication.class, args);
+    }
+
+}
