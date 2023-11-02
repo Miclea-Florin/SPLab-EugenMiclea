8a4fc6f8bd3be00e451bfd5d80e5e2e4fd6a8933
Miclea-Florin <95932163+Miclea-Florin@users.noreply.github.com>
2023-11-02 10:32:23 +0200

    added AlignStrategy interface

---
+++ src/main/java/com/example/splabeugenmiclea/Classes/AlignStrategy.java
@@ -0,0 +1,6 @@
+package com.example.splabeugenmiclea.Classes;
+
+public interface AlignStrategy {
+
+    public void setAlignStrategy();
+}
--- src/main/java/com/example/splabeugenmiclea/Classes/Paragraph.java
+++ src/main/java/com/example/splabeugenmiclea/Classes/Paragraph.java
@@ -2,13 +2,22 @@ package com.example.splabeugenmiclea.Classes;
 
 public class Paragraph implements Element{
     private String name;
+    AlignStrategy as = null;
     public Paragraph(String name){
         this.name = name;
     }
 
     @Override
     public void print(){
-        System.out.println("Paragraph: " + name);
+        if(as ==null)
+            System.out.println("Paragraph: " + name);
+        else if(as instanceof AlignLeft)
+            System.out.println("# Paragraph: " + name);
+        else if(as instanceof AlignRight)
+            System.out.println("Paragraph: " + name+"#");
+        else if(as instanceof AlignCenter)
+            System.out.println("# Paragraph: " + name+"#");
+
     }
 
     @Override
@@ -25,4 +34,9 @@ public class Paragraph implements Element{
     public Element get(int i) {
         throw new UnsupportedOperationException();
     }
+
+    public void setAlignStrategy(AlignStrategy a){
+        as = a;
+    }
+
 }
